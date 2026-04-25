a58729ed7f0425345ca199afeb02fd087737135d
aspathak2705 <137284420+aspathak2705@users.noreply.github.com>
2026-04-25 17:46:10 +0530

    fusion logic Updated to v2

--- core/fusion.py
+++ core/fusion.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 
 def compute_iou(box1, box2):
     x1, y1, x2, y2 = box1
@@ -31,7 +32,7 @@ def analyze_changes(boxes_t1, boxes_t2, iou_thresh=0.3):
         found = False
 
         for j, b2 in enumerate(boxes_t2):
-            if compute_iou(b1, b2) > iou_thresh:
+            if(compute_iou(b1, b2) > iou_thresh) or (centroid_distance(b1, b2) < 30):
                 found = True
                 matched_t2.add(j)
                 unchanged.append(b2)
@@ -64,3 +65,13 @@ def draw_fusion(image_path, new, removed, unchanged, output_path):
         cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 1)
 
     cv2.imwrite(output_path, img)
+
+
+def centroid(box):
+    x1, y1, x2, y2 = box
+    return ((x1 + x2) // 2, (y1 + y2) // 2)
+
+def centroid_distance(box1, box2):
+    c1 = centroid(box1)
+    c2 = centroid(box2)
+    return math.sqrt((c1[0] - c2[0])**2 + (c1[1] - c2[1])**2)
